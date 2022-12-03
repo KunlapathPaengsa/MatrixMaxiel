b30f7d9175210121048685e1c74074c7353b6073
Kunlapath Paengsa <kunlapath.paengsa@gmail.com>
2022-12-03 23:46:59 +0700

    Add bitmap

--- MatrixMaxiel/Program.cs
+++ MatrixMaxiel/Program.cs
@@ -1,20 +1,26 @@
 ﻿using MatrixMaxiel;
 using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+//using System.IO;
 
 Stopwatch stopWatch = new Stopwatch();
 stopWatch.Start();
-//int[] data = { 1, 2, 3, 4, 5, 6 };
-int[] data = { 1, 2, 3, 4, 5, 6, 7 };
-//var x = IEnumerator
 //var column = 3;
-
+//int[] data = { 1, 2, 3, 4, 5, 6 };
+//int[] data = { 1, 2, 3, 4, 5, 6, 7 };
+var data = Enumerable.Range(1, 6).ToArray();
+//int[][] jaggedArray = new int[2][];
+int width = 720, height = 576;
 
 var testMatrix = new Matrix<int>(3, data);
-var resultMatrix = testMatrix.ToMatrix();
 
+var resultBitmap = GenerateRandomImage();
 
-//var x = Math.PI;
-DisplayMatrix();
+//var resultMatrix = testMatrix.ToMatrix();
+
+//DisplayMatrix();
+DisplayBitmap();
 
 stopWatch.Stop();
 Console.Write(stopWatch.ElapsedMilliseconds);
@@ -27,14 +33,67 @@ void DisplayMatrix()
     {
         for (int j = 0; j < testMatrix.Column; j++)
         {
-            //AddMatrix(i, j);
-            Console.Write(resultMatrix[i, j]);
+            //Console.Write(resultMatrix[i, j]);
             if (j < testMatrix.Column - 1)
             {
                 Console.Write(", ");
             }
         }
         Console.WriteLine();
+    }
+    Console.WriteLine($"Dim({testMatrix.Row},{testMatrix.Column})");
+}
+
+//void GenerateRandomImage()
+Bitmap GenerateRandomImage()
+{
+    //int width = testMatrix.Column * 200, height = testMatrix.Row * 200;
+
+    //bitmap
+    Bitmap bmp = new Bitmap(width, height);
+
+    //random number
+    Random rand = new Random();
+
+    //create random pixels
+    for (int y = 0; y < height; y++)
+    {
+        for (int x = 0; x < width; x++)
+        {
+            if ((x > 100 && x < 200) && (y > 100 && y < 200))
+            {
+
+                //generate random ARGB value
+                int a = rand.Next(256);
+                int r = rand.Next(256);
+                int g = rand.Next(256);
+                int b = rand.Next(256);
+
+                //set ARGB value
+                bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+            }
+        }
+    }
+
+
+    //load bmp in picturebox1
+    //pictureBox1.Image = bmp;
+
+    //save (write) random pixel image
+    bmp.Save("C:\\Users\\Maxiel\\Pictures\\RandomImageMatrix.png", ImageFormat.Jpeg);
+
+    return bmp;
+
+}
+void DisplayBitmap()
+{
+    for (int j = 0; j < height; j++)
+    {
+        for (int i = 0; i < width; i++)
+        {
+            Console.Write($"({i}, {j})");
+        }
+        Console.WriteLine(" | ");
     }
     Console.WriteLine($"Dim({testMatrix.Row},{testMatrix.Column})");
 }
