b073c0e0c89bcf56a8b46ae9ba776434b2b4d45c
Kunlapath Paengsa <kunlapath.paengsa@gmail.com>
2022-12-04 09:47:48 +0700

    Add bitmap get and save image

---
+++ EditImage/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+Stopwatch stopWatch = new Stopwatch();
+stopWatch.Start();
+var bmp = (Bitmap)Image.FromFile(@"C:\Users\Maxiel\Pictures\RandomImageMatrix.png");
+
+Random rand = new Random();
+
+for (int y = 100; y < 200; y++)
+{
+    for (int x = 300; x < 400; x++)
+    {
+        //generate random ARGB value
+        int a = rand.Next(256);
+        int r = rand.Next(256);
+        int g = rand.Next(256);
+        int b = rand.Next(256);
+
+        //set ARGB value
+        bmp.SetPixel(x, y, Color.FromArgb(a, r, g, b));
+
+    }
+}
+stopWatch.Stop();
+
+DisplayBitmap();
+
+
+bmp.Save("C:\\Users\\Maxiel\\Pictures\\RandomImageMatrix2.png", ImageFormat.Jpeg);
+
+
+void DisplayBitmap()
+{
+    //int width = 720, height = 576;
+    //for (int j = 0; j < height; j++)
+    //{
+    //    for (int i = 0; i < width; i++)
+    //    {
+    //        Console.Write($"({i}, {j})");
+    //    }
+    //    Console.WriteLine(" | ");
+    //}
+    Console.Write(stopWatch.ElapsedMilliseconds);
+    Console.Write(" Milliseconds");
+
+}
